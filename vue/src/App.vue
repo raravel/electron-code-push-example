7ab6fa12448b4d347769810517afbfb2ea848eef
raravel <youn@tree-some.dev>
2021-07-21 14:08:37 +0900

    vue example

---
+++ vue/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld :msg="ipcTest"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+const { ipcRenderer } = window.require('electron');
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  computed: {
+	ipcTest() {
+      return ipcRenderer.sendSync('test-msg', 'hello');
+	},
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
